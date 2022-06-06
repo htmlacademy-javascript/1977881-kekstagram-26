0edc9a48d9e33ae3334a52b11c45ef1dbdd2ddf6
Gaika_kat <brova.kat@gmail.com>
2022-06-06 09:03:57 +0300

    Убрала вывод в консоль

--- js/main.js
+++ js/main.js
@@ -9,4 +9,4 @@ getRandomPositiveInteger(3,5);
 function checkStringLength (string, length) {
   return string.length <= length;
 }
-console.log(checkStringLength('2',16));
+checkStringLength('2',16);
